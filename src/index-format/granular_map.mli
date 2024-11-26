aff7d9a63c13b5165ebbc8ee6f83f2f099fc915c
Lucccyo <cha.git@mailo.fr>
2024-11-26 12:00:38 +0100

    add granular marshal

---
+++ src/index-format/granular_map.mli
@@ -0,0 +1,31 @@
+open Granular_marshal
+
+module type OrderedType = sig
+  type t
+  val compare : t -> t -> int
+end
+
+module Make (Ord : OrderedType) : sig
+  type 'a s
+  type 'a t = 'a s link
+  type key = Ord.t
+
+  val empty: string t
+  val bindings: 'a t -> (key * 'a) list
+  val add: key -> 'a -> 'a t -> 'a t
+  val singleton: key -> 'a -> 'a t
+  val remove: key -> 'a t -> 'a t
+  val union: (key -> 'a -> 'a -> 'a option) -> 'a t -> 'a t -> 'a t
+  val cardinal: 'a t -> int
+  val find: key -> 'a t -> 'a
+  val find_opt: key -> 'a t -> 'a option
+  val iter: (key -> 'a -> unit) -> 'a t -> unit
+  val fold: (key -> 'a -> 'acc -> 'acc) -> 'a t -> 'acc -> 'acc
+  val map: ('a -> 'b) -> 'a t -> 'b t
+  val is_empty: 'a t -> bool
+  val mem: key -> 'a t -> bool
+  val update: key -> ('a option -> 'a option) -> 'a t -> 'a t
+  val schema: Granular_marshal.iter ->
+    (key -> 'a -> unit) -> 'a s Granular_marshal.link -> unit
+end
+
